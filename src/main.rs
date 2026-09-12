06141f72a5dd246e2c9af4c6ee09b8905adfec09
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2026-09-12 10:31:18 -0700

    Complete draw instruction
    IBM logo rom is now working

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
 const WIDTH: usize = 64;
 const HEIGHT: usize = 32;
 const MEM_SIZE: usize = 4096;
+const MEM_START: usize = 0x200;
 const MINIFB_WHITE: u32 = 0x00FFFFFF;
 const MINIFB_BLACK: u32 = 0x00000000;
 
 fn read_program_into_memory(ram: &mut [u8; MEM_SIZE]) {
     let program_bytes = fs::read("IBM Logo.ch8").expect("Cannot read file");
 
-    let mut ram_counter = 0x200;
+    let mut ram_counter = MEM_START;
     for byte in program_bytes {
         // println!("byte content is {}", byte);
         ram[ram_counter] = byte;
@@ -25,7 +26,7 @@ fn read_program_into_memory(ram: &mut [u8; MEM_SIZE]) {
 fn main() {
     let mut display_buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut ram: [u8; MEM_SIZE] = [0; MEM_SIZE];
-    let mut program_counter: usize = 0x200;
+    let mut program_counter: usize = MEM_START;
     let mut index_register: u16 = 0;
     // let stack: u16 = 4096;
     let mut var_registers: [u8; 16] = [0; 16];
@@ -59,9 +60,9 @@ fn main() {
         let cur_instruction_high = ram[program_counter];
         let cur_instruction_low = ram[program_counter + 1];
 
-        let high_first_nibble = cur_instruction_high / 0b1111;
+        let high_first_nibble = cur_instruction_high / 0b00010000;
         let high_second_nibble = (cur_instruction_high & 0b00001111) as u8;
-        let low_first_nibble = cur_instruction_low / 0b1111;
+        let low_first_nibble = cur_instruction_low / 0b00010000;
         let low_second_nibble = (cur_instruction_low & 0b00001111) as u8;
 
         // dbg!(program_counter, cur_instruction_high, high_second_nibble);
@@ -69,7 +70,7 @@ fn main() {
         match high_first_nibble {
             0x0 => match cur_instruction_low {
                 0xE0 => {
-                    println!("E: cleared display");
+                    // println!("00E0: cleared display");
                     display_buffer.fill(0);
                     window
                         .update_with_buffer(&display_buffer, WIDTH, HEIGHT)
@@ -84,7 +85,6 @@ fn main() {
                 let jmp_pos: usize =
                     (high_second_nibble as usize) * 0b00010000 + (cur_instruction_low as usize);
                 program_counter = jmp_pos;
-                println!("1: jump to {jmp_pos}");
             }
             0x6 => {
                 let register_index = high_second_nibble as usize;
@@ -98,20 +98,57 @@ fn main() {
             }
             0xA => {
                 index_register =
-                    (high_second_nibble as u16) * 0b00010000 + (cur_instruction_low as u16);
-                program_counter += 2
+                    (high_second_nibble as u16) * 0b0000000100000000 + (cur_instruction_low as u16);
+                program_counter += 2;
+                // println!("A: set index register to {:#X}", index_register);
             }
             0xD => {
+                var_registers[0xF] = 0;
+
                 let x_pos = var_registers[high_second_nibble as usize] % (WIDTH as u8);
                 let y_pos = var_registers[low_first_nibble as usize] % (HEIGHT as u8);
 
-                let buffer_pos: usize = ((x_pos as usize) + (y_pos as usize) * WIDTH) as usize;
-                display_buffer[buffer_pos] = 0x00FFFFFF;
+                let n = low_second_nibble;
+
+                // dbg!(x_pos, y_pos);
+
+                // println!("D: Draw at ({x_pos}, {y_pos}) with {n} lines");
+
+                for down_offset in 0..n {
+                    if y_pos + down_offset >= HEIGHT as u8 {
+                        break;
+                    }
+
+                    let mut mem_buffer: u8 = ram[index_register as usize + down_offset as usize];
+                    // dbg!(index_register, down_offset, mem_buffer);
+                    // println!("index + down = {}", index_register as usize + down_offset as usize);
+
+                    for right_offset in 0..8 {
+                        if x_pos + right_offset >= WIDTH as u8 {
+                            break;
+                        }
+
+                        let buffer_pos: usize = ((x_pos as usize)
+                            + right_offset as usize
+                            + (y_pos as usize + down_offset as usize) * WIDTH)
+                            as usize;
+
+                        if mem_buffer > 0b01111111 {
+                            if display_buffer[buffer_pos] == MINIFB_WHITE {
+                                var_registers[0xF] = 1;
+                                display_buffer[buffer_pos] = MINIFB_BLACK;
+                            } else {
+                                display_buffer[buffer_pos] = MINIFB_WHITE;
+                            }
+                        }
+                        mem_buffer = mem_buffer << 1;
+                    }
+                }
 
                 window
                     .update_with_buffer(&display_buffer, WIDTH, HEIGHT)
                     .unwrap();
-                println!("D: draw at {x_pos}, {y_pos} with value {low_second_nibble}");
+
                 program_counter += 2;
             }
             _ => {
